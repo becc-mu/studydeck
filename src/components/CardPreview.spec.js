ddf316b4b1e18f4006a6367ad852e73880877fa7
rebecca mulugeta <becc.mulugeta@gmail.com>
2020-09-21 23:06:30 +0100

    set up spec file and 2 passing tests

---
+++ src/components/CardPreview.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { CardPreview } from './CardPreview'
+import { render, fireEvent } from '@testing-library/react'
+
+it('renders the expected term', () => {
+  const expectedTerm = 'This is a test'
+  const { getByText } = render(<CardPreview term={expectedTerm} />)
+  expect(getByText(expectedTerm)).toBeInTheDocument()
+})
+
+it('flips card when clicked', () => {
+  const expectedTerm = 'This is term'
+  const expectedDef = 'This is def'
+  const { getByText, queryByText } = render(
+    <CardPreview term={expectedTerm} definition={expectedDef} />,
+  )
+  expect(getByText(expectedTerm)).toBeInTheDocument()
+
+  const flipButton = getByText(/show/i)
+  fireEvent.click(flipButton)
+  expect(getByText(expectedDef)).toBeInTheDocument()
+  expect(queryByText(expectedTerm)).not.toBeInTheDocument()
+})
